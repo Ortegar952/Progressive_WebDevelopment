0ef760e38dc8224fe29b1142ab43045771392155
Raul <Ortegar95@gmail.com>
2019-09-04 19:34:49 -0400

    new files

---
+++ module/main.js
@@ -0,0 +1,9 @@
+import {sumAll, subtractAll, multiplyAll, divideAll, findModule} from './math.js';
+console.log('Sum All', sumAll(6,3));
+console.log('Subtract All', subtractAll(100,3));
+console.log('Multiply All', multiplyAll(100,3));
+console.log('Divide All', divideAll(100,3));
+console.log('Finding Modules', findModule(100,3));
+
+
+
---
+++ module/math.js
@@ -0,0 +1,17 @@
+let sumAll = (a,b) =>{
+    return a + b;
+}
+let subtractAll = (a,b) =>{
+    return a - b;
+}
+let divideAll = (a,b) =>{
+    return a / b;
+}
+let multiplyAll = (a,b) =>{
+    return a * b;
+}
+let findModule = (a,b) =>{
+    return a % b;
+}
+
+export {sumAll, subtractAll, divideAll ,multiplyAll, findModule }
