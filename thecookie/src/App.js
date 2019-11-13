0d075ebcffabe3c1d4de38b0ba3b42887ba5a665
Raul <Ortegar95@gmail.com>
2019-11-12 19:14:47 -0500

    new app

--- thecookie/src/App.js
+++ thecookie/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import Cookie from './components/Cookie'
+import React, { Component } from 'react';
+import Cookie from './components/Cookie';
+import Calendar from 'react-calendar';
+
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Cookie />
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: new Date(),
+  }
+  onChange = date => this.setState({date})
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Cookie />
+        </header>
+        {/* <div className = 'Calendar-Comp'>
+          <Mycalendar />
+        </div> */}
+        <div className='Calendar'>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ thecookie/src/components/AddEvent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+ 
+class Mycalendar extends Component {
+  state = {
+    date: new Date(),
+  }
+ 
+  onChange = date => this.setState({ date })
+ 
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
+export default Mycalendar;
